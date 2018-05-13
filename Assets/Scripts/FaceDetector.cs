cdac2a44d5282f10d118f6e0ee888b3169decf1c
Emre <emrsmsrli1@gmail.com>
2018-05-13 20:12:58 +0300

    c

--- Assets/Scripts/FaceDetector.cs
+++ Assets/Scripts/FaceDetector.cs
@@ -1,37 +1,57 @@
 ﻿using System;
 using System.Collections;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class FaceDetector : MonoBehaviour {
-    /*AndroidJavaClass jc = new AndroidJavaClass("com.unity3d.player.UnityPlayer"); 
-AndroidJavaObject jo = jc.GetStatic<AndroidJavaObject>("currentActivity");*/
-
     public Camera cam;
-    private Texture2D texture;
     private int framec = 0;
     private AndroidJavaObject context;
     private AndroidJavaClass frameDist;
     private RenderTexture rt;
+    private Rect rect;
+    private Texture2D texture;
+    private bool anyOpenCvPlugins;
+
+    private int w;
+    private int h;
+
+    private const int ResMult = 4;
 
     // Use this for initialization
     void Start() {
-        rt = new RenderTexture(Screen.width, Screen.height, 24);
+        w = Screen.width / ResMult;
+        h = Screen.height / ResMult;
+        rt = new RenderTexture(w, h, 24);
+        rect = new Rect(0, 0, w, h);
+        texture = new Texture2D(w, h, TextureFormat.RGB24, false);
         cam.targetTexture = rt;
-        texture = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
         Debug.Log("starting detection");
 
         using (var jc = new AndroidJavaClass("com.unity3d.player.UnityPlayer")) {
             context = jc.GetStatic<AndroidJavaObject>("currentActivity");
             frameDist = new AndroidJavaClass("tr.edu.iyte.vrxd.unityhook.FrameDistributer");
-            frameDist.CallStatic("loadPlugins", context);
+            anyOpenCvPlugins = frameDist.CallStatic<bool>("loadPlugins", context);
         }
     }
 
     // Update is called once per frame
     void Update() {
         framec++;
-        if (framec % 20 == 0)
+
+        int count = frameDist.CallStatic<int>("getFrameObj", framec / 30);
+        if (count != -1) {
+            for (int i = 0; i < count; i++) {
+                
+            }
+        }
+
+        if (framec % 30 == 0)
+            // TODO record current camera angle and frameId
             StartCoroutine(Detect());
+
+        // Camera.main.transform.rotation
+        // TODO ask for frame ready then collect and draw if any shapes
     }
 
     IEnumerator Detect() {
@@ -40,19 +60,37 @@ AndroidJavaObject jo = jc.GetStatic<AndroidJavaObject>("currentActivity");*/
         yield return new WaitForEndOfFrame();
 
         cam.Render();
+
+        var ms = sw.ElapsedMilliseconds;
+
         var temp = RenderTexture.active;
         RenderTexture.active = rt;
+
+        var ms2 = sw.ElapsedMilliseconds;
+
         // put buffer into texture
-        texture.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+        texture.ReadPixels(rect, 0, 0);
+
+        var ms3 = sw.ElapsedMilliseconds;
 
         RenderTexture.active = temp;
         yield return null;
 
         texture.Apply();
 
+        var ms4 = sw.ElapsedMilliseconds;
+
         yield return null;
 
-        frameDist.CallStatic("distribute", texture.EncodeToPNG());
-        Debug.Log("elapsed: " + sw.ElapsedMilliseconds + "ms");
+        using (var mat = new AndroidJavaObject("org.opencv.core.MatOfByte", texture.EncodeToJPG(70))) {
+            frameDist.CallStatic("distribute", framec / 30, mat);
+        }
+
+        yield return null;
+        Debug.Log("\nelapsed render camera: " + ms + "ms\n" +
+                  "elapsed switch textures: " + (ms2 - ms) + "ms\n" +
+                  "elapsed read pixels: " + (ms3 - ms2) + "ms\n" +
+                  "elapsed switch textures and apply: " + (ms4 - ms3) + "ms\n" +
+                  "elapsed total: " + sw.ElapsedMilliseconds + "ms");
     }
 }
