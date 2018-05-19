503ed5c2c6ed2ea20b4cdebd9bba14d17c6d6938
Emre <emrsmsrli1@gmail.com>
2018-05-19 23:08:44 +0300

    refactor face detector and frame

---
+++ Assets/Scripts/Frame.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Frame {
+    public Quaternion CameraAngle { get; set; }
+    public int FrameCount { get; set; }
+
+    public Frame(int frameCount, Quaternion cameraAngle) {
+        CameraAngle = cameraAngle;
+        FrameCount = frameCount;
+    }
+}
