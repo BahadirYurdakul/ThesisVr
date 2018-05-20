1abeca1e25e0a11c822e76a93f88d321e5dc8086
Emre <emrsmsrli1@gmail.com>
2018-05-20 23:29:21 +0300

    refactor framedistributer

--- Assets/Scripts/FrameDistributer.cs
+++ Assets/Scripts/FrameDistributer.cs
@@ -1,32 +1,32 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 using UnityEngine;
 
-public class FaceDetector : MonoBehaviour {
+public class FrameDistributer : MonoBehaviour {
     private int framec = 0;
     private AndroidJavaObject context;
     private AndroidJavaClass frameDist;
     private RenderTexture rt;
     private Rect rect;
     private Texture2D texture;
-    private List<Frame> frames;
-    private GameObject container;
+
+    private readonly List<Frame> frames = new List<Frame>();
+    private readonly List<GameObject> rects = new List<GameObject>();
 
     private int _w;
     private int _h;
 
     private const int ResMult = 4;
 
-    public GameObject g;
+    public GameObject rectSprite;
+    public GameObject container;
     public Camera cam;
 
     // Use this for initialization
     void Start() {
-        frames = new List<Frame>();
-        container = GameObject.FindGameObjectWithTag("VideoSpriteContainer");
-
         _w = Screen.width / ResMult;
         _h = Screen.height / ResMult;
         rt = new RenderTexture(_w, _h, 24);
@@ -48,8 +48,11 @@ public class FaceDetector : MonoBehaviour {
         var frameId = framec / 30;
 
         var nearPlane = cam.nearClipPlane;
-        foreach (var model in frames) {
-            var allPluginShapes = frameDist.CallStatic<string>("getFrameShapes", model.FrameCount);
+        foreach (var frame in frames) {
+            if (frame.IsDrawn)
+                continue;
+
+            var allPluginShapes = frameDist.CallStatic<string>("getFrameShapes", frame.Id);
             if (allPluginShapes == null || allPluginShapes.Equals(""))
                 continue;
             log("shapes:", allPluginShapes);
@@ -62,8 +65,19 @@ public class FaceDetector : MonoBehaviour {
                             var y = float.Parse(attrs[2], CultureInfo.InvariantCulture);
                             var width = float.Parse(attrs[3], CultureInfo.InvariantCulture);
                             var height = float.Parse(attrs[4], CultureInfo.InvariantCulture);
-                            var worldPoint = cam.ScreenToWorldPoint(new Vector3(x + width / 2, _h - (y + height / 2), nearPlane));
-                            Instantiate(g, worldPoint, model.CameraAngle, container.transform);
+
+                            var originalRotation = cam.transform.localRotation;
+                            cam.transform.localRotation = frame.CameraRotation;
+                            // todo fix
+                            var worldPoint =
+                                cam.ScreenToWorldPoint(new Vector3(x + width / 2, _h - (y + height / 2), nearPlane + 1));
+
+                            if (rects.Any(r => Vector3.Distance(r.transform.position, worldPoint) < .2f))
+                                break;
+
+                            rects.Add(Instantiate(rectSprite, worldPoint,
+                                Quaternion.LookRotation(-cam.transform.forward), container.transform));
+                            cam.transform.localRotation = originalRotation;
                             break;
                         case "text":
                             break;
@@ -72,6 +86,8 @@ public class FaceDetector : MonoBehaviour {
                     }
                 }
             }
+
+            frame.IsDrawn = true;
         }
 
         if (framec % 30 == 0)
@@ -104,8 +120,8 @@ public class FaceDetector : MonoBehaviour {
 
         texture.Apply();
         yield return null;
-        
-        frames.Add(new Frame(frameId, Camera.main.transform.rotation));
+
+        frames.Add(new Frame(frameId, cam.transform.localRotation));
 
         using (var mat = new AndroidJavaObject("org.opencv.core.MatOfByte", texture.EncodeToJPG(70))) {
             frameDist.CallStatic("distribute", frameId, mat);
