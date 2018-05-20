60beea2922a958789fdd943aee1f3470b6d8c690
Emre <emrsmsrli1@gmail.com>
2018-05-20 23:29:30 +0300

    save scene

--- Assets/Scripts/VideoControlButtonsScripts/PlaypauseButtonScript.cs
+++ Assets/Scripts/VideoControlButtonsScripts/PlaypauseButtonScript.cs
@@ -4,60 +4,55 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlaypauseButtonScript : MonoBehaviour {
-
     public float lookTime = 3f;
     private bool isLooking = false;
     private float timer = 0f;
     public GameObject MagnetButton;
+    public GameObject frameDistributer;
     public Canvas VideoDisplayCanvas;
 
-    void OnDisable()
-    {
+    void OnDisable() {
         resetTheGaze();
     }
 
-    void OnEnable()
-    {
+    void OnEnable() {
         resetTheGaze();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if (isLooking)
-        {
-            if (MagnetButton.GetComponent<MagnetSensor>().isPressedMagnetButton)
-                CursorClick();
+    void Update() {
+        if (MagnetButton.GetComponent<MagnetSensor>().isPressedMagnetButton) {
+            CursorClick();
+            return;
+        }
+
+        if (isLooking) {
             timer += Time.deltaTime;
-            if (timer >= lookTime)
-            {
+            if (timer >= lookTime) {
                 CursorClick();
             }
         }
     }
 
-    public void CursorEnter()
-    {
+    public void CursorEnter() {
         timer = 0f;
         isLooking = true;
         Debug.Log("CursorEnter");
     }
 
-    public void CursorExit()
-    {
+    public void CursorExit() {
         resetTheGaze();
         Debug.Log("CursorExit");
     }
 
-    public void CursorClick()
-    {
+    public void CursorClick() {
         timer = 0f;
         VideoDisplayCanvas.GetComponent<VideoControlsManager>().OnPlayPause();
+        frameDistributer.SetActive(!frameDistributer.activeSelf);
         Debug.Log("CursorClick");
     }
 
-    private void resetTheGaze()
-    {
+    private void resetTheGaze() {
         timer = 0f;
         isLooking = false;
     }
